150a06e513fe873fbbadd5fc77dad006185a64c8
venkatyerru <venkatnaidu2715@gmail.com>
2026-07-16 02:16:41 -0400

    playing with the Routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user", (req, res)=>{
-    res.send({firstName: "Venkat"});
-})
+app.get("/user/:userId", (req, res)=>{
+    console.log(req.params);
+    res.send(`{query params we got ${req.params.userId}}`);
+});
 app.post("/user", (req, res)=>{
     //saving data to DB
     res.send("Data Sucessfully stored to the database");
-})
+});
 
 app.delete("/user", (req, res)=>{
     //saving data to DB
     res.send("Dlete to the database");
-})
+});
 app.use("/test",(req, res)=>{
 res.send("hello from the namaste dashboard");
 });
