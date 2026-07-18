e634140006e11a738b8f89b5265ba24eadea256b
venkatyerru <venkatnaidu2715@gmail.com>
2026-07-18 02:55:55 -0400

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,24 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId", (req, res)=>{
-    console.log(req.params);
-    res.send(`{query params we got ${req.params.userId}}`);
-});
-app.post("/user", (req, res)=>{
-    //saving data to DB
-    res.send("Data Sucessfully stored to the database");
-});
+app.use(
+  "/user",
 
-app.delete("/user", (req, res)=>{
-    //saving data to DB
-    res.send("Dlete to the database");
-});
-app.use("/test",(req, res)=>{
-res.send("hello from the namaste dashboard");
-});
+  (req, res,next) => {
+    // res.send("hello this is user");
+    next();
+    res.send("hello");
+  },
+  (req, res) => {
+    res.send("hello this user handler 2");
+  }
+);
 
-app.listen(3000,()=>{
-    console.log("server is listening successfully");
+// app.get("/user/:userId", (req, res)=>{
+//     console.log(req.params);
+//     res.send(`{query params we got ${req.params.userId}}`);
+// });
+// app.post("/user", (req, res)=>{
+//     //saving data to DB
+//     res.send("Data Sucessfully stored to the database");
+// });
+
+// app.delete("/user", (req, res)=>{
+//     //saving data to DB
+//     res.send("Dlete to the database");
+// });
+// app.use("/test",(req, res)=>{
+// res.send("hello from the namaste dashboard");
+// });
+
+app.listen(3000, () => {
+  console.log("server is listening successfully");
 });
